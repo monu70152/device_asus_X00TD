235f1f2b4de62aff9537be3a76ed5a05bf384114
N4veenNK <naveen4264nk@gmail.com>
2022-02-08 10:59:39 +0530

    X00TD: `update raven fp to feb`
    Signed-off-by: N4veenNK <naveen4264nk@gmail.com>

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -34,10 +34,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
 
 # MAINTAINER STUFF
 ARCANA_MAINTAINER := Naveen
